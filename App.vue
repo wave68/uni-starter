994c3cea76411f69c2d8ae0aaf6715e958cfa478
wave <1107501874@qq.com>
2022-11-16 10:08:09 +0800

    init

---
+++ App.vue
@@ -0,0 +1,81 @@
+<script>
+	import initApp from '@/common/appInit.js';
+	import openApp from '@/common/openApp.js';
+	import checkIsAgree from '@/pages/uni-agree/utils/uni-agree.js';
+	export default {
+		globalData: {
+			searchText: '',
+			appVersion: {},
+			config: {},
+			$i18n: {},
+			$t: {}
+		},
+		onLaunch: function() {
+			console.log('App Launch')
+			this.globalData.$i18n = this.$i18n
+			this.globalData.$t = str => this.$t(str)
+
+			initApp();
+			
+			// #ifdef H5
+				openApp() //创建在h5端全局悬浮引导用户下载app的功能
+			// #endif
+			// #ifdef APP-PLUS
+			plus.runtime.setBadgeNumber(0) //修改图标角标数字
+			//checkIsAgree(); APP端暂时先用原生默认生成的。目前，自定义方式启动vue界面时，原生层已经请求了部分权限这并不符合国家的法规
+			// #endif
+
+			// #ifdef H5
+			// checkIsAgree(); // 默认不开启。目前全球，仅欧盟国家有网页端同意隐私权限的需要。如果需要可以自己去掉注视后生效
+			// #endif
+
+			// #ifdef APP-PLUS
+			//idfa有需要的用户在应用首次启动时自己获取存储到storage中
+			//https://ask.dcloud.net.cn/article/36107
+			/*if(~plus.storage.getItem('idfa')){
+				plus.device.getInfo({//需要勾选IDFA
+					success:function(e){  
+						console.log('idfa =  '+JSON.stringify(e.idfa));  
+					},
+					fail:function(e){
+						console.log('getDeviceInfo failed: '+JSON.stringify(e));  
+					}  
+				});
+			}*/
+			// #endif
+			uni.onPushMessage((res) => {
+				console.log("收到推送消息：",res) //监听推送消息
+				// #ifdef APP-PLUS
+				// uni.createPushMessage({
+				// 	title:res.data.title,
+				// 	content:res.data.content
+				// })
+				// #endif
+				// #ifdef H5
+				uni.showModal({
+					title: res.data.title,
+					content: res.data.content,
+					showCancel:false,
+					success: function (res) {
+						if (res.confirm) {
+							console.log('用户点击确定');
+						} else if (res.cancel) {
+							console.log('用户点击取消');
+						}
+					}
+				});
+				// #endif
+			})
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
